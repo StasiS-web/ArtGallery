0d24bcdc54151d36988526257e2c93f9b9738179
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-02-28 23:37:09 +0000

    Setup a Theme

---
+++ ArtGallery/ArtGallery.Web/Views/Shared/_FooterPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ArtGallery.Web.wwwroot.Shared
+{
+    public class _FooterPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
