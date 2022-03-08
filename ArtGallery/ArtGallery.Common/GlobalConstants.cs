f9c83bd8fa9079be5820997854b284297cfd0d60
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-03-08 14:59:45 +0000

    Contants Updates

--- ArtGallery/ArtGallery.Common/GlobalConstants.cs
+++ ArtGallery/ArtGallery.Common/GlobalConstants.cs
@@ -67,8 +67,8 @@
 
         public static class Images
         {
-            // (Images Resourse from rawpixel)
-            public const string Error404 = "https://res.cloudinary.com/dnvg6uuxl/image/upload/c_fit,h_325,w_1200/v1645821424/app_gallery/error404-jpg_xnvavw.jpg";
+            // (Images is for Personal and non-Commercial. Use only for this project. Need repalcement for real app.)
+            public const string Error404 = "https://res.cloudinary.com/dnvg6uuxl/image/upload/s--XnOApIFH--/c_fit,h_706,w_1200/v1646650092/app_gallery/error_404_wnxcl7.jpg";
 
             // Art Store (Images Resourse from rawpixel)
             public const string MrPeacock = "https://res.cloudinary.com/dnvg6uuxl/image/upload/c_fit,h_300,w_300/v1645821443/app_gallery/peacock-jpeg_d9gei3.jpg";
@@ -121,39 +121,6 @@
             public const string PasswordDisplay = "Password";
             public const string ConfirmPasswordDisplay = "Confiirm Password";
             public const string PageContentDisplayName = "Page Content";
-        }
-
-        public static class ErrorMessages
-        {
-            public const string InvalidUsername = "User with Username: {0} does not exist.";
-            public const string InvalidUserId = "User with ID: {0} does not exist.";
-            public const string UserFullname = "Fullname is invalid. It should be between 90 and 500 characters long.";
-            public const string PasswordValidation = "Password is not valid, because Password and Confirm Password didn't match.";
-            public const string EmptyField = "The field should not be empthy. It is required to have a content.";
-            public const string PageContentLength = "Page content should be between 1000 and 15000 characters long.";
-
-            // Events
-            public const string EventName = "Event Name should be between 8 and 60 characters long.";
-            public const string EventDescription = "Description should be between 250 and 1500 characters long.";
-            public const string EventCapacity = "Event Capacity depends from the Exhibition Hall Type. It should be between 5 and 100 people.";
-
-            // Blog
-            public const string Title = "Title must be between 5 and 70 characters long.";
-            public const string Content = "Content must be between 600 and 3500 characters long.";
-            public const string Comment = "Comment should be between 5 and 150 characters long.";
-
-            // Arts
-            public const string PaintingName = "Painting Name should be between 10 and 35 characters long.";
-            public const string ArtsDescription = "Description must be between 150 and 1000 chracters long.";
-
-            // Arts and Blog
-            public const string AuthorsName = "Author name must be between 50 and 100 characters long.";
-        }
-
-        public static class OperationalMessages
-        {
-            public const string SuccessfullyBookedEvent = "Your choosen event has been succefully booked!";
-            public const string SuccessfullyCheckoutOrder = "Your order has been successfully checkout!";
         }
     }
 }
---
+++ ArtGallery/ArtGallery.Common/MessageConstants.cs
@@ -0,0 +1,42 @@
+﻿namespace ArtGallery.Common
+{
+    public static class MessageConstants
+    {
+        public const string WarningMessage = "WarningMessage";
+
+        public static class ErrorMessages
+        {
+            public const string InvalidUsername = "User with Username: {0} does not exist.";
+            public const string InvalidUserId = "User with ID: {0} does not exist.";
+            public const string UserFullname = "Fullname is invalid. It should be between 90 and 500 characters long.";
+            public const string UsernameLength = "Username should not be null. It should be between 5 and 20 characters long.";
+            public const string PasswordValidation = "Password and Confirm Password didn't match.";
+            public const string PasswordLength = "Password should be between 8 and 16 character long.";
+            public const string EmptyField = "The field should not be empthy. It is required to have a content.";
+            public const string PageContentLength = "Page content should be between 1000 and 15000 characters long.";
+
+            // Events
+            public const string EventName = "Event Name should be between 8 and 60 characters long.";
+            public const string EventDescription = "Description should be between 250 and 1500 characters long.";
+            public const string EventCapacity = "Event Capacity depends from the Exhibition Hall Type. It should be between 5 and 100 people.";
+
+            // Blog
+            public const string Title = "Title must be between 5 and 70 characters long.";
+            public const string Content = "Content must be between 600 and 3500 characters long.";
+            public const string Comment = "Comment should be between 5 and 150 characters long.";
+
+            // Arts
+            public const string PaintingName = "Painting Name should be between 10 and 35 characters long.";
+            public const string ArtsDescription = "Description must be between 150 and 1000 chracters long.";
+
+            // Arts and Blog
+            public const string AuthorsName = "Author name must be between 50 and 100 characters long.";
+        }
+
+        public static class OperationalMessages
+        {
+            public const string SuccessfullyBookedEvent = "Your choosen event has been succefully booked!";
+            public const string SuccessfullyCheckoutOrder = "Your order has been successfully checkout!";
+        }
+    }
+}
