3247e20ca6d4cf5833de8e679ec2458e37ade1f0
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-03-09 02:12:52 +0000

    Add new User View Model

---
+++ ArtGallery/ArtGallery.Web.ViewModels/Users/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ArtGallery.Web.ViewModels.Users
+{
+    public class UserViewModel
+    {
+        public string UserId { get; set; }
+
+        public string FullName { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Gender { get; set; }
+
+        public string UrlImage { get; set; }
+
+        public string Email { get; set; }
+    }
+}
