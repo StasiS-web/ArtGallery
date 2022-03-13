63858653982bcd14cc9e27891e0ba44b8c8fc393
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-03-13 05:52:08 +0000

    Add Art Order View Model

---
+++ ArtGallery/ArtGallery.Web.ViewModels/ArtStore/ArtOrderViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace ArtGallery.Web.ViewModels.ArtStore
+{
+    public class ArtOrderViewModel
+    {
+        public string OrderDate { get; set; }
+
+        public string UserId { get; set; }
+
+        public string User { get; set; }
+
+        public int ArtId { get; set; }
+
+        public string PaintingName { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
