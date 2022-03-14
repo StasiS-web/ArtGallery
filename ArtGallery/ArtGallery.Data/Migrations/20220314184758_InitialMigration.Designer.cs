d54d6bad67eea39cf225dbd20ef6f7b8cb112127
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-03-14 19:03:49 +0000

    Modifications

--- ArtGallery/ArtGallery.Data/Migrations/20220314184758_InitialMigration.Designer.cs
+++ ArtGallery/ArtGallery.Data/Migrations/20220314184758_InitialMigration.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ArtGallery.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20220313164055_InitialMigration")]
+    [Migration("20220314184758_InitialMigration")]
     partial class InitialMigration
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
--- ArtGallery/ArtGallery.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ ArtGallery/ArtGallery.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -18,15 +18,12 @@ namespace ArtGallery.Web.Areas.Identity.Pages.Account
     [AllowAnonymous]
     public class LoginModel : PageModel
     {
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
 
-        public LoginModel(SignInManager<ApplicationUser> signInManager, 
-            ILogger<LoginModel> logger,
-            UserManager<ApplicationUser> userManager)
+        public LoginModel(SignInManager<IdentityUser> signInManager, 
+            ILogger<LoginModel> logger)
         {
-            _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
         }
