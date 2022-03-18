720a0c79c4b21945001a54a8090938c804ad3138
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-03-18 22:56:52 +0000

    Add ErrorController

---
+++ ArtGallery/ArtGallery.Web/Controllers/ErrorController.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using ArtGallery.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Plugins;
+
+namespace ArtGallery.Web.Controllers
+{
+    public class ErrorController : BaseController
+    {
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        public IActionResult Error()
+        {
+            return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+        }
+
+        [Route("/Error/Error/{code:int}")]
+        public IActionResult Error(int code)
+        {
+            return this.View();
+        }
+
+        [Route("~/Error/404")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error404()
+        {
+            return this.View(new ErrorViewModel
+                { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+        }
+
+        [Route("~/Error/500")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error500()
+        {
+            return this.View(new ErrorViewModel
+            { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+        }
+    }
+}
