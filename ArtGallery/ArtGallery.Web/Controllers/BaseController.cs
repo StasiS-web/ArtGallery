8ce0e7d17674b032ab53ff8d9096b3af7aec21ba
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-03-18 22:55:35 +0000

    Add BaseController

---
+++ ArtGallery/ArtGallery.Web/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtGallery.Web.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+    }
+}
