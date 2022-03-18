d0df35180370ab983edad9367dacec6dae1dc168
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-03-18 22:51:14 +0000

    Add UserEditModel

---
+++ ArtGallery/ArtGallery.Web.ViewModels/Users/UserEditModel.cs
@@ -0,0 +1,32 @@
+﻿namespace ArtGallery.Web.ViewModels.Users
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using static ArtGallery.Common.GlobalConstants.ArtGalleryUser;
+    using static ArtGallery.Common.GlobalConstants.DisplayNames;
+
+    public class UserEditModel
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(FullNameMaxLenth)]
+        [MinLength(FullNaneMinLenth)]
+        [Display(Name = FirstNameDisplayName)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(FullNameMaxLenth)]
+        [MinLength(FullNaneMinLenth)]
+        [Display(Name = LastNameDisplayName)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(UsernameMaxLength)]
+        [MinLength(UsernameMinLength)]
+        [Display(Name = UsernameDisplay)]
+        public string UserName { get; set; }
+    }
+}
