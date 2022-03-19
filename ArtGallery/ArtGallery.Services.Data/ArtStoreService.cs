af95ce18d5e0878031aec7286bc005ce3202398f
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-03-19 04:02:10 +0000

    Add ArtStoreService

---
+++ ArtGallery/ArtGallery.Services.Data/ArtStoreService.cs
@@ -0,0 +1,41 @@
+﻿namespace ArtGallery.Services.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using ArtGallery.Data.Models;
+    using ArtGallery.Data.Repositories.Contracts;
+    using ArtGallery.Services.Data.Contracts;
+    using ArtGallery.Services.Mapping;
+    using ArtGallery.Web.ViewModels.ArtStore;
+
+    public class ArtStoreService : IArtStoreService
+    {
+        private IAppRepository storeRepo;
+
+        public ArtStoreService(IAppRepository storeRepo)
+        {
+            this.storeRepo = storeRepo;
+        }
+
+        public IEnumerable<ArtStoreViewModel> GetAll()
+        {
+            return this.storeRepo
+                .All<ArtStore>()
+                .To<ArtStoreViewModel>()
+                .ToList();
+        }
+
+        public IEnumerable<int> GetById<T>(int id)
+        {
+            var blogPost = this.storeRepo
+                     .All<ArtStoreViewModel>()
+                     .Where(x => x.ArtId == id)
+                     .FirstOrDefault();
+
+            return (IEnumerable<int>)blogPost;
+        }
+    }
+}
---
+++ ArtGallery/ArtGallery.Services.Data/Contracts/IArtStoreService.cs
@@ -0,0 +1,17 @@
+﻿namespace ArtGallery.Services.Data.Contracts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using ArtGallery.Data.Models;
+    using ArtGallery.Web.ViewModels.ArtStore;
+
+    public interface IArtStoreService
+    {
+        IEnumerable<int> GetById<T>(int id);
+
+        IEnumerable<ArtStoreViewModel> GetAll();
+    }
+}
