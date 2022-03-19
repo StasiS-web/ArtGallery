94360d77a9beb7d53c5c74bea0a4b72403b23bd0
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-03-19 15:06:50 +0000

    Add new ViewModel

---
+++ ArtGallery/ArtGallery.Web.ViewModels/Users/UserRolesViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ArtGallery.Web.ViewModels.Users
+{
+    using ArtGallery.Data.Models.Enumeration;
+
+    public class UserRolesViewModel
+    {
+        public string UserId { get; set; }
+
+        public UserRole Name { get; set; }
+
+        public string[] RoleIds { get; set; }
+    }
+}
