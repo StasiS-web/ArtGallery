8b2d776f5ad1e05ea67278332ba745d2ab21c70a
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-03-21 23:15:07 +0000

    Add Administration Controllers

---
+++ ArtGallery/ArtGallery.Web/Areas/Administration/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtGallery.Web.Areas.Administration.Controllers
+{
+
+    [Authorize(Roles = GlobalConstants.Roles.AdministratorRoleName)]
+    [Area("Administration")]
+    public class BaseController : Controller
+    {
+    }
+}
---
+++ ArtGallery/ArtGallery.Web/Areas/Administration/Controllers/UserController.cs
@@ -0,0 +1,99 @@
+﻿using ArtGallery.Web.ViewModels.Users;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ArtGallery.Web.Areas.Administration.Controllers
+{
+    public class UserController : BaseController
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<ArtGalleryUser> userManager;
+        private readonly IUserService userService;
+
+        public UserController(RoleManager<IdentityRole> roleManager, UserManager<ArtGalleryUser> userManager, IUserService userService)
+        {
+            this.roleManager = roleManager;
+            this.userManager = userManager;
+            this.userService = userService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ManageUsers()
+        {
+            var users = this.userService.GetUsers();
+
+            return View(users);
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> Roles(string userId)
+        {
+            var user = await userService.GetUserById(userId);
+            var model = new UserRolesViewModel()
+            {
+                UserId = userId,
+                Name = $"{user.FirstName} {user.LastName}"
+            };
+
+            ViewBag.RoleItems = roleManager.Roles
+                .ToList()
+                .Select(r => new SelectListItem()
+                {
+                    Text = r.Name,
+                    Value = r.Id,
+                    Selected = userManager.IsInRoleAsync(user, r.Name).Result
+                }).ToList();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Roles(UserRolesViewModel model)
+        {
+            return Ok(model);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Edit(string id)
+        {
+            var model = await userService.GetUserToEdit(id);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UserEditViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (await userService.UpdateUser(model))
+            {
+                ViewData[MessageConstants.OperationalMessages] = "Succefully Written Down";
+            }
+            else
+            {
+                ViewData[MessageConstants.OperationalMessages] = "Oops! An error occurred.";
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> CreateRole()
+        {
+            await roleManager.CreateAsync(new IdentityRole()
+            {
+                Name = "ArtGallery"
+            });
+
+            return Ok();
+        }
+    }
+}
