48b3a73fa9d916d13ba18c026be18f65aaf6dfbf
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-03-21 23:19:41 +0000

    Add UserListViewModel

---
+++ ArtGallery/ArtGallery.Web.ViewModels/Users/UserListViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ArtGallery.Web.ViewModels.Users
+{
+    public class UserListViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; } 
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
