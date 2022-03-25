14fe60f3773934808f4a563d4a5f87b41cbae875
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-03-25 21:01:20 +0000

    Add ArtDetailsViewModel

---
+++ ArtGallery/ArtGallery.Web.ViewModels/ArtStore/ArtDetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ArtGallery.Web.ViewModels.ArtStore
+{
+    public class ArtDetailsViewModel
+    {
+        public int ArtId { get; set; }
+
+        public string PaintingName { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public IFormFile UrlImage { get; set; }
+
+        public decimal? Price { get; set; }
+
+        public string Description { get; set; }
+    }
+}
