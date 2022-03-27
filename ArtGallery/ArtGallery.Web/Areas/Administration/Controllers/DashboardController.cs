582a4509e1e3868b4b4f664ea2bf16c82bc836d3
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-03-27 18:46:12 +0100

    Add new Dashboard View and Controller

---
+++ ArtGallery/ArtGallery.Web/Areas/Administration/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArtGallery.Web.Areas.Administration.Controllers
+{
+    public class DashboardController : AdministrationController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
