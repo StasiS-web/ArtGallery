916c8bbfa86a889f0a467b5dad9c6434b8e4a967
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-03-27 20:32:59 +0100

    Add new Create Functions for Blog Post

---
+++ ArtGallery/ArtGallery.Web/Areas/Administration/Controllers/BlogController.cs
@@ -0,0 +1,41 @@
+﻿using ArtGallery.Services.Data.Administrator.Contracts;
+using ArtGallery.Web.ViewModels.Administrator;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtGallery.Web.Areas.Administration.Controllers
+{
+    public class BlogController : AdministrationController
+    {
+        private readonly IAdminBlogPostService adminService;
+
+        public BlogController(IAdminBlogPostService adminService)
+        {
+            this.adminService = adminService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("/Blog/CreateBlog")]
+        [Authorize]
+        public async Task<IActionResult> CreateBlog(BlogPostCreateInputModel model)
+        {
+            var userId = this.User.FindFirst(model.UrlImage.FileName);
+
+            if (model.UrlImage != null)
+            {
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return View(model);
+        }
+
+    }
+}
