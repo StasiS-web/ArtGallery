a98f48b128c473a2b3f465e8e8e61372611eff41
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-03-28 06:37:34 +0100

    Modified Controllers and Update the Error Handling by using StatusCodes

--- ArtGallery/ArtGallery.Web/Controllers/ErrorController.cs
+++ ArtGallery/ArtGallery.Web/Controllers/ErrorController.cs
@@ -7,37 +7,36 @@ namespace ArtGallery.Web.Controllers
 {
     public class ErrorController : BaseController
     {
-
-        public IActionResult Index()
-        {
-            return this.View();
-        }
-
+        [Route("/Error/Error")]
         public IActionResult Error()
         {
             return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
         }
 
-        [Route("/Error/Error/{code:int}")]
-        public IActionResult Error(int code)
-        {
-            return this.View();
-        }
-
-        [Route("~/Error/404")]
+        [Route("/Error/404")]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error404()
         {
-            return this.View(new ErrorViewModel
-                { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+            var errorModel = new ErrorViewModel
+            { 
+                  StatusCode = StatusCodes.Status404NotFound,
+                  RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier,
+            };
+
+            return View(errorModel);
         }
 
-        [Route("~/Error/500")]
+        [Route("/Error/500")]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error500()
         {
-            return this.View(new ErrorViewModel
-            { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+            var errorModel = new ErrorViewModel
+            {
+                StatusCode = StatusCodes.Status500InternalServerError,
+                RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier,
+            };
+
+            return View(errorModel);
         }
     }
 }
--- ArtGallery/ArtGallery.Web/Controllers/HomeController.cs
+++ ArtGallery/ArtGallery.Web/Controllers/HomeController.cs
@@ -2,7 +2,9 @@ namespace ArtGallery.Web.Controllers
 {
     using System.Collections.Generic;
     using System.Diagnostics;
+    using System.Dynamic;
     using ArtGallery.Web.ViewModels;
+    using ArtGallery.Web.ViewModels.BlogPosts;
     using ArtGallery.Web.ViewModels.Events;
     using ArtGallery.Web.ViewModels.Home;
     using Microsoft.AspNetCore.Mvc;
@@ -11,65 +13,46 @@ namespace ArtGallery.Web.Controllers
 
     public class HomeController : BaseController
     {
-        // private readonly ILogger<HomeController> _logger;
         private readonly IEventService eventService;
         private readonly IBlogPostService blogPostService;
 
-        public HomeController(ILogger<HomeController> logger, IEventService eventService, IBlogPostService blogPostService)
+        public HomeController(IEventService eventService, IBlogPostService blogPostService)
         {
             this.eventService = eventService;
             this.blogPostService = blogPostService;
         }
 
+        [HttpGet("/")]
         public IActionResult Index()
         {
             this.ViewData[MessageConstants.OperationalMessages] = SuccessfullyRunningToaster;
 
-            return View();
-        }
-
-        [HttpGet("/Home/Index")]
-        public IActionResult ListUpcomingEvents()
-        {
-            var upcomingEvents = new IndexViewModel()
+            dynamic model = new ExpandoObject();
+            var modelView = new IndexViewModel
             {
-               AllUpcomingEvents = (IEnumerable<UpcomingEventViewModel>)this.eventService.GetUpcomingByIdAsync<UpcomingEventViewModel>(SeededDataCounts.Events),
+                AllUpcomingEvents = this.eventService.GetUpcomingByIdAsync<EventViewModel>(model.eventService.EventId),
+                AllLatestBlogPost = this.blogPostService.GetLatestBlogAsync(model.blogPostService.BlogId),
             };
 
-            return View(upcomingEvents);
-        }
-
-        [HttpPost]
-        public IActionResult ListUpcomingEvents(UpcomingEventViewModel model)
-        {
-            return Ok(model);
-        }
-
-        [HttpGet("/Home/Index")]
-        public IActionResult LatestBlogPost()
-        {
-            var latestBlogPost = new IndexViewModel()
-            {
-                AllLatestBlogPost = (IEnumerable<LatestBlogPostViewModel>)this.blogPostService.GetLatestBlogAsync(SeededDataCounts.BlogPost),
-            };
-
-            if (latestBlogPost == null)
-            {
-                throw new ArgumentNullException(LatestPost);
-            }
-
-            return View(latestBlogPost);
-        }
-
-        [HttpPost]
-        public IActionResult LatestBlogPost(LatestBlogPostViewModel model)
-        {
-            return Ok(model);
+            return View(modelView);
         }
 
         public IActionResult Privacy()
         {
             return View();
         }
+
+        [Route("/Home/{code:int}")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error(int? code = null)
+        {
+            if (code == StatusCodes.Status404NotFound)
+            {
+                return Redirect($"/Error/{StatusCodes.Status404NotFound}");
+            }
+
+            return Redirect($"/Error/{StatusCodes.Status500InternalServerError}");
+        }
+
     }
 }
