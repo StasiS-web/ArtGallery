cbbf44a9c0bb76396ca9f45f0e0d5387029e66f6
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-03-28 21:46:21 +0100

    Add Blog Post Edit View

---
+++ ArtGallery/ArtGallery.Web.ViewModels/Administrator/BlogPostEditViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace ArtGallery.Web.ViewModels.Administrator
+{
+    using System.ComponentModel.DataAnnotations;
+    using static ArtGallery.Common.GlobalConstants.BlogPost;
+
+    public class BlogPostEditViewModel
+    {
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        [MinLength(TitleMinLength)]
+        public string Title { get; set; }
+
+        [Required]
+        public string UrlImage { get; set; }
+
+        [Required]
+        [MaxLength(ContentMaxLength)]
+        [MinLength(ContentMinLength)]
+        public string Content { get; set; }
+    }
+}
