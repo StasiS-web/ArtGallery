28d37e633475ec589ea5f8e91020203780d25db7
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-04-02 00:33:17 +0100

    Add ObjectMapping

---
+++ ArtGallery/ArtGallery.Services.Mapping/ObjectMappingExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace ArtGallery.Services.Mapping
+{
+    using System;
+
+    public static class ObjectMappingExtensions
+    {
+        public static T To<T>(this object exist)
+        {
+            if (exist == null)
+            {
+                throw new ArgumentNullException(nameof(exist));
+            }
+
+            return AutoMapperConfig.MapperInstance.Map<T>(exist);
+        }
+    }
+}
