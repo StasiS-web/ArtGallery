5e1368bb96fcae6764dbd139ac1a7b2b4eb94a10
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-04-02 04:55:56 +0100

    Modified Views

--- ArtGallery/ArtGallery.Web/Areas/Identity/Pages/Error.cshtml.cs
+++ ArtGallery/ArtGallery.Web/Areas/Identity/Pages/Error.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,11 +11,11 @@ namespace ArtGallery.Web.Areas.Identity.Pages
     {
         public string RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(this.RequestId);
 
         public void OnGet()
         {
-            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier;
         }
     }
 }
