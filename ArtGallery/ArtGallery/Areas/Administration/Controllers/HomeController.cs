02aa76896af127af9ed25a9c9f80ec99414d3766
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-04-09 13:11:13 +0100

    Add Admin Home Page

---
+++ ArtGallery/ArtGallery/Areas/Administration/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArtGallery.Areas.Administration.Controllers
+{
+    public class HomeController : AdministrationController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
