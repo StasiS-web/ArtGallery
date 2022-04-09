61402e33e3d009154d275b3e9fb8f3e091b12187
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-04-09 13:15:22 +0100

    Add Admin BlogConttroller

---
+++ ArtGallery/ArtGallery/Controllers/BlogController.cs
@@ -0,0 +1,53 @@
+﻿namespace ArtGallery.Controllers
+{
+
+    using ArtGallery.Core.Contracts;
+    using ArtGallery.Core.Models.BlogPosts;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    public class BlogController : BaseController
+    {
+        private readonly IBlogPostService blogPost;
+
+        public BlogController(IBlogPostService blog)
+        {
+            this.blogPost = blog;
+        }
+
+        [Route("/Blog")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Index(int? sortId, int blogId)
+        {
+            if (sortId != null)
+            {
+                var blogPost = this.blogPost.GetById<BlogPostViewModel>(sortId.Value);
+
+                if (blogPost == null)
+                {
+                    return new StatusCodeResult(404);
+                }
+            }
+
+            this.ViewData["CurrentSort"] = sortId;
+
+            var blogPosts = await this.blogPost.GetAll<BlogPostViewModel>(sortId, blogId);
+            var blogList = blogPosts.ToList();
+            ViewBag.AllBlog = blogList;
+
+            return View();
+        }
+
+        [Route("/Event/PostDetails/{blogId}")]
+        public IActionResult PostDetails(int blogId)
+        {
+            if (blogId == null)
+            {
+                return new StatusCodeResult(404);
+            }
+
+            var blogPost = this.blogPost.GetBlogPostDetailsByIdAsync<BlogPostViewModel>(blogId);
+
+            return View(blogPost);
+        }
+    }
+}
