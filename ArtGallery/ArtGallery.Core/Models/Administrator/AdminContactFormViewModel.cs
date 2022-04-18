ca8b2997610968f98f615fc2953199e6c9b326e4
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-04-18 04:31:22 +0300

    Modified View and Delete Unused Files

--- ArtGallery/ArtGallery.Core/Models/Administrator/AdminContactFormViewModel.cs
+++ ArtGallery/ArtGallery.Core/Models/Administrator/AdminContactFormViewModel.cs
@@ -2,15 +2,16 @@
 {
     using ArtGallery.Core.Mapping.Contracts;
     using ArtGallery.Infrastructure.Data.Models;
+    using AutoMapper;
     using System.ComponentModel.DataAnnotations;
     using static ArtGallery.Common.GlobalConstants.ContactForm;
 
-    public class AdminContactFormViewModel : IMapFrom<ContactForm>
+    public class AdminContactFormViewModel : IHaveCustomMappings
     {
         [Required]
-        [MaxLength( FullNameMaxLength)]
+        [MaxLength(FullNameMaxLength)]
         [MinLength(FullNameMinLength)]
-        public string FullName { get; set; }
+        public string FullName { get; set; } 
 
         [Required]
         [EmailAddress]
@@ -25,5 +26,11 @@
         [MaxLength(MessageMaxLength)]
         [MinLength(MessageMinLength)]
         public string Message { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<AdminContactFormViewModel, ContactForm>()
+                 .ForMember(des => $"{des.FirstName} {des.LastName}", opts => opts.MapFrom(x => x.FullName));
+        }
     }
 }
