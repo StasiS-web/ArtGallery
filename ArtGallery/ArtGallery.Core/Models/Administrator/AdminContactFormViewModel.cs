2bf12885c061c62b0d37efb51ad2373a2e15dbac
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-04-18 03:26:14 +0300

    Add Contacts View Models

---
+++ ArtGallery/ArtGallery.Core/Models/Administrator/AdminContactFormViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace ArtGallery.Core.Models.Administrator
+{
+    using ArtGallery.Core.Mapping.Contracts;
+    using ArtGallery.Infrastructure.Data.Models;
+    using System.ComponentModel.DataAnnotations;
+    using static ArtGallery.Common.GlobalConstants.ContactForm;
+
+    public class AdminContactFormViewModel : IMapFrom<ContactForm>
+    {
+        [Required]
+        [MaxLength( FullNameMaxLength)]
+        [MinLength(FullNameMinLength)]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(SubjectMaxLength)]
+        [MinLength(SubjectMinLength)]
+        public string Subject { get; set; }
+
+        [Required]
+        [MaxLength(MessageMaxLength)]
+        [MinLength(MessageMinLength)]
+        public string Message { get; set; }
+    }
+}
