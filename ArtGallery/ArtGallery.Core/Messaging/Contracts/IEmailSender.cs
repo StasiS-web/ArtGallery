3d08d697c73c519b6d8a7f510e43b2653b3ef917
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-04-18 03:24:43 +0300

    Add Messaging

---
+++ ArtGallery/ArtGallery.Core/Messaging/Contracts/IEmailSender.cs
@@ -0,0 +1,13 @@
+﻿namespace ArtGallery.Core.Messaging.Contracts
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(
+            string from,
+            string fromName,
+            string to,
+            string subject,
+            string htmlContent,
+           IEnumerable<EmailAttachment> attachments = null);
+    }
+}
