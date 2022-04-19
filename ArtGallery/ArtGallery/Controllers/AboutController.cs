ac18b3c603c5890156c0eec01463876465782b51
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-04-19 17:44:12 +0300

    Add About Controller

---
+++ ArtGallery/ArtGallery/Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using ArtGallery.Core.Models.FaqEntity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtGallery.Controllers
+{
+    public class AboutController : BaseController
+    {
+        private readonly IAboutService aboutService;
+
+        public AboutController(IAboutService aboutService)
+        {
+            this.aboutService = aboutService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var faq = aboutService.GetAllFaqsAsync<FaqViewModel>();
+
+            return View(faq);
+        }
+    }
+}
