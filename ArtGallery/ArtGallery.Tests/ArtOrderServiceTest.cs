025f1d493e32cd93c266961aeee695b194a90dd4
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-08-16 02:07:38 +0100

    Add Tests

---
+++ ArtGallery/ArtGallery.Tests/ArtOrderServiceTest.cs
@@ -0,0 +1,68 @@
+﻿using ArtGallery.Core.Contracts;
+using ArtGallery.Infrastructure.Data.Repositories;
+using ArtGallery.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+using ArtGallery.Tests.Common;
+using ArtGallery.Core.Services;
+
+namespace ArtGallery.Tests
+{
+    public class ArtOrderServiceTest
+    {
+        private Mock<IAppRepository> _repo;
+        private Mock<IArtOrderService> _artOrderService;
+        private ApplicationDbContext _context;
+        public ArtOrderServiceTest()
+        {
+            _repo = new Mock<IAppRepository>();
+            _artOrderService = new Mock<IArtOrderService>();
+            _context = new ApplicationDbContext(new DbContextOptionsBuilder<ApplicationDbContext>()
+         .UseInMemoryDatabase(databaseName: "artOrderDb")
+         .Options);
+        }
+
+        [Fact]
+        public void CreateOrder_Test()
+        {
+            // Assert
+            _repo.Setup(x => x.AddAsync(ObjectGenerator.GetArtOrderViewModelObject()));
+            _artOrderService.Setup(x => x.CreateOrder(ObjectGenerator.GetArtOrderViewModelObject()));
+
+            // Act
+            var service = new ArtOrder(_repo.Object, _context);
+
+            // Verify
+            _artOrderService.Verify(x => x.CreateOrder(ObjectGenerator.GetArtOrderViewModelObject()), Times.Never);
+        }
+
+
+        [Fact]
+        public void ReceivedOrder_Test()
+        {
+            // Assert
+            _artOrderService.Setup(x => x.ReceivedOrder("1")).Returns(Task.Run(() => true));
+
+            // Act
+            var service = new ArtOrder(_repo.Object, _context);
+
+            // Verify
+            _artOrderService.Verify(x => x.ReceivedOrder("1"), Times.Never);
+        }
+
+        [Fact]
+        public void CancleOrder_Test()
+        {
+            // Assert
+            _artOrderService.Setup(x => x.CancleOrder("1")).Returns(Task.Run(() => true));
+
+            // Act
+            var service = new ArtOrder(_repo.Object, _context);
+
+            // Verify
+            _artOrderService.Verify(x => x.CancleOrder("1"), Times.Never);
+        }
+    }
+}
---
+++ ArtGallery/ArtGallery.Tests/CommentsServiceTest.cs
@@ -0,0 +1,78 @@
+﻿using ArtGallery.Core.Contracts;
+using ArtGallery.Core.Models.BlogPosts;
+using ArtGallery.Core.Services;
+using ArtGallery.Infrastructure.Data.Repositories;
+using ArtGallery.Infrastructure.Data;
+using ArtGallery.Tests.Common;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using ArtGallery.Infrastructure.Data.Models;
+
+namespace ArtGallery.Tests
+{
+    public class CommentsServiceTest
+    {
+        private Mock<IAppRepository> _repo;
+        private Mock<ICommentsService> _commentsService;
+        private ApplicationDbContext _context;
+        public CommentsServiceTest()
+        {
+            _repo = new Mock<IAppRepository>();
+            _commentsService = new Mock<ICommentsService>();
+            _context = new ApplicationDbContext(new DbContextOptionsBuilder<ApplicationDbContext>()
+        .UseInMemoryDatabase(databaseName: "CommentsDb")
+        .Options);
+        }
+
+        [Fact]
+        public void CreateArtAsync_Test()
+        {
+            // Assert
+            _repo.Setup(x => x.AddAsync(ObjectGenerator.GetBlogCommentObject()));
+            _commentsService.Setup(x => x.CreateAsync(1, 1, "1", "1"));
+
+            // Act
+            var service = new CommentsService(_repo.Object);
+
+            // Verify
+            _commentsService.Verify(x => x.CreateAsync(1, 1, "1", "1"), Times.Never);
+        }
+
+        [Fact]
+        public void DeleteAsync_Test()
+        {
+            // Assert
+            _repo.Setup(x => x.All<BlogComment>());
+            _repo.Setup(x => x.Delete<BlogComment>(ObjectGenerator.GetBlogCommentObject()));
+            _commentsService.Setup(x => x.DeleteAsync(1));
+
+            // Act
+            var service = new CommentsService(_repo.Object);
+
+            // Verify
+            _commentsService.Verify(x => x.DeleteAsync(1), Times.Never);
+        }
+
+        [Fact]
+        public void GetBlogIdByCommentsAsync_Test()
+        {
+            // Assert
+            _repo.Setup(x => x.All<BlogCommentViewModel>()).Returns(new List<BlogCommentViewModel> {
+            ObjectGenerator.GetBlogCommentViewModelObject()}.AsQueryable());
+
+            _commentsService.Setup(x => x.GetBlogIdByCommentsAsync(1));
+
+            // Act
+            var service = new CommentsService(_repo.Object);
+
+            // Verify
+            _commentsService.Verify(x => x.GetBlogIdByCommentsAsync(1), Times.Never);
+        }
+    }
+}
