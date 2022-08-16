4fadff5e8d1f8a948216ef7609c49cb4e2870ee7
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-08-16 17:42:31 +0100

    Implement TwoFactorAuthentication

--- ArtGallery/ArtGallery/Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
+++ ArtGallery/ArtGallery/Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
@@ -2,35 +2,37 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
+using ArtGallery.Core.Messaging.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using ArtGallery.Infrastructure.Data.Models;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Net.Mail;
 
 namespace ArtGallery.Areas.Identity.Pages.Account
 {
+    // Code change by bhavin.
     public class LoginWith2faModel : PageModel
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<LoginWith2faModel> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly IEmailSender _emailSender;
+        private readonly IConfiguration _configuration;
 
         public LoginWith2faModel(
             SignInManager<ApplicationUser> signInManager,
             UserManager<ApplicationUser> userManager,
-            ILogger<LoginWith2faModel> logger)
+            ILogger<LoginWith2faModel> logger,
+            ApplicationDbContext dbContext,
+            IConfiguration configuration)
         {
             _signInManager = signInManager;
             _userManager = userManager;
             _logger = logger;
+            _context = dbContext;
+            _configuration = configuration;
         }
 
         /// <summary>
@@ -58,6 +60,8 @@ namespace ArtGallery.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            public string AuthenticatorCode { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -88,7 +92,10 @@ namespace ArtGallery.Areas.Identity.Pages.Account
 
             ReturnUrl = returnUrl;
             RememberMe = rememberMe;
-
+            var token = await _userManager.GenerateTwoFactorTokenAsync(user, "Email");
+            var model = new InputModel();
+            model.AuthenticatorCode = token;
+            ViewData["AuthenticatorCode"] = token;
             return Page();
         }
 
@@ -109,9 +116,15 @@ namespace ArtGallery.Areas.Identity.Pages.Account
 
             var authenticatorCode = Input.TwoFactorCode.Replace(" ", string.Empty).Replace("-", string.Empty);
 
-            var result = await _signInManager.TwoFactorAuthenticatorSignInAsync(authenticatorCode, rememberMe, Input.RememberMachine);
+            //var result = await _signInManager.TwoFactorAuthenticatorSignInAsync(authenticatorCode, rememberMe, Input.RememberMachine);
+            var result = await _signInManager.TwoFactorSignInAsync("Email", authenticatorCode, rememberMe, Input.RememberMachine);
 
-            var userId = await _userManager.GetUserIdAsync(user);
+
+            var roles = await _userManager.GetRolesAsync(user);
+            if (roles.Any(x => x.Contains(GlobalConstants.AdministratorRoleName)))
+            {
+                returnUrl = "/Administration/Home";
+            }
 
             if (result.Succeeded)
             {
@@ -127,6 +140,9 @@ namespace ArtGallery.Areas.Identity.Pages.Account
             {
                 _logger.LogWarning("Invalid authenticator code entered for user with ID '{UserId}'.", user.Id);
                 ModelState.AddModelError(string.Empty, "Invalid authenticator code.");
+                var token = await _userManager.GenerateTwoFactorTokenAsync(user, "Email");
+                Input.AuthenticatorCode = token;
+                ViewData["AuthenticatorCode"] = token;
                 return Page();
             }
         }
