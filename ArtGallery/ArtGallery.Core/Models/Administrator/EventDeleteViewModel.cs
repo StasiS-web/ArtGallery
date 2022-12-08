f3f4057a90e995cd1ffc4369b754a90b90402d9f
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-12-08 20:31:14 +0000

    Add new Administrator View Model

---
+++ ArtGallery/ArtGallery.Core/Models/Administrator/EventDeleteViewModel.cs
@@ -0,0 +1,37 @@
+﻿namespace ArtGallery.Core.Models.Administrator
+{
+    using ArtGallery.Infrastructure.Data.Models;
+    using ArtGallery.Core.Mapping.Contracts;
+    using ArtGallery.Infrastructure.Data.Models.Enumeration;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+
+    public class EventDeleteViewModel : IMapFrom<Event>
+    {
+        public int EventId { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Date { get; set; }
+
+        public EventType Type { get; set; }
+
+        public List<SelectListItem> EventType { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "1", Text = "InPerson" },
+            new SelectListItem { Value = "2", Text = "Online" },
+        };
+
+        public TicketType TicketSelection { get; set; }
+
+        public List<SelectListItem> TicketType { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "1", Text = "Free" },
+            new SelectListItem { Value = "2", Text = "Paid" },
+        };
+
+
+        public string Description { get; set; }
+    }
+}
---
+++ ArtGallery/ArtGallery.Core/Models/Administrator/FaqDeleteViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ArtGallery.Core.Models.Administrator
+{
+    using ArtGallery.Infrastructure.Data.Models;
+    using ArtGallery.Core.Mapping.Contracts;
+    public class FaqDeleteViewModel : IMapFrom<FaqEntity>
+    {
+        public int FaqId { get; set; }
+
+        public string Question { get; set; }
+
+        public string Answer { get; set; }
+
+    }
+}
